305ab8843ad8604062a753f9d63693abd0b7cf1c
Noah Cooper <noahcoop@umich.edu>
2019-10-12 23:29:01 -0400

    devpost commit

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
-import {Dimensions, ImageBackground, StyleSheet, Text, View, Modal, AsyncStorage, Image, Alert, TouchableOpacity, KeyboardAvoidingView,} from 'react-native';
+import {Dimensions, ImageBackground, StyleSheet, Text, View, Modal, AsyncStorage, ActivityIndicator, Alert, TouchableOpacity, KeyboardAvoidingView,} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas';
 import ViewShot from 'react-native-view-shot'
 import ImageEditor from "@react-native-community/image-editor";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import List from './List'
+import Progress from './Progress'
 import {createSwitchNavigator, createAppContainer, SwitchNavigator, NavigationEvents} from "react-navigation";
 import {createStackNavigator} from "react-navigation-stack"
 
 import PieChart from 'react-native-chart-kit';
+
+console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
+
 
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3({accessKeyId:'AKIA3BSGO4O2CBIRNSMG', secretAccessKey:'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', region:'us-east-1'});
@@ -26,11 +30,12 @@ class Home extends Component
           minX: 0,
           minY: 0,
           maxX: Dimensions.get('window').width,
-          maxY: Dimensions.get('window').height - 200,
+          maxY: Dimensions.get('window').height - 100,
           nutrition: {},
           food: "",
           viewModal: false,
-          data: []
+          data: [],
+          indicating: false
         };
       }
 
@@ -86,15 +91,16 @@ class Home extends Component
 
     clear = () => {
       this.pad.clear()
-      this.setState({minX: 0, minY: 0, maxX: Dimensions.get('window').width, maxY:  Dimensions.get('window').height - 200 })
+      this.setState({minX: 0, minY: 0, maxX: Dimensions.get('window').width, maxY:  Dimensions.get('window').height - 100 })
     }
 
     captureScreen = async (x, y) => {
+      this.setState({indicating: true})
       const height = this.state.maxY - this.state.minY
       const width = this.state.maxX - this.state.minX
       const cropData = {
         offset: {x: x, y: y},
-        size: {width: Dimensions.get('window').width, height: Dimensions.get('window').height - 200},
+        size: {width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100},
         resizeMode: 'contain',
       }
 
@@ -113,18 +119,21 @@ class Home extends Component
                         fetch("https://mhacks-12.herokuapp.com/checkFood", {
                           method: 'POST',
                           body: JSON.stringify({
-                            imageURL: "https://www.thespruceeats.com/thmb/xZU__qGyThuUAq8mTosBd1cmLAs=/1333x1000/smart/filters:no_upscale()/juicy-baked-burgers-3052097-7-5b1054308e1b6e0036bc6cd1.jpg"
+                            imageURL: "https://flip-storage.s3.amazonaws.com/mhacks12/" + urlKey + "/image.jpg"
                           })
                         })
                         .then(response => response.json())
                         .then(async data => {
+                          await this.clear() 
                           this.setState({
                             nutrition: data.nutrition,
                             food: data.food,
-                            viewModal: true
+                            viewModal: true,
+                            indicating: false
                           })
                         })
                   } else {
+                    this.setState({indicating: false, viewModal: false})
                     alert('Error while sending the image to S3')
                 } 
               } 
@@ -139,15 +148,7 @@ class Home extends Component
         return (
           <View style={styles.container}>
     
-          <View style={{flexDirection: 'row', width: Dimensions.get('window').width}}>
-            <TouchableOpacity onPress={() => {}} style={styles.capture}>
-              <Icon size = {24} color = {'black'} name="star"></Icon>
-            </TouchableOpacity>
-            <View style = {{width: Dimensions.get('window').width - 210}}></View>
-            <TouchableOpacity onPress={() => {this.props.navigation.navigate("List")}} style={styles.capture}>
-              <Icon size = {24} color = {'black'} name="list"></Icon>
-            </TouchableOpacity>
-          </View>
+          
 
           {this.state.camera && <RNCamera
             ref={ref => {
@@ -156,56 +157,78 @@ class Home extends Component
             style={styles.preview}
             type={RNCamera.Constants.Type.back}
             flashMode={RNCamera.Constants.FlashMode.off}
-          />}
+          >
+            <View style={{flexDirection: 'row', width: Dimensions.get('window').width, justifyContent: 'space-evenly'}}>
+            <TouchableOpacity onPress={() => {this.props.navigation.navigate("Progress")}} style={styles.capture}>
+              <Icon size = {24} color = {'black'} name="star"></Icon>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Icon size = {36} color = {'black'} name="camera-alt"></Icon>
+           </TouchableOpacity>
+            <TouchableOpacity onPress={() => {this.props.navigation.navigate("List")}} style={styles.capture}>
+              <Icon size = {24} color = {'black'} name="list"></Icon>
+            </TouchableOpacity>
+            
+            
+          </View>
+          </RNCamera>
+          }
           {!this.state.camera && 
+          <View>
           <ViewShot ref="viewShot" options={{ format: "jpg", quality: 0.9 }}>
            
-            <Modal
+          <Modal
             animationType="slide"
             transparent = {true}
             visible = {this.state.viewModal}
-           >
-            <View  style = {{width: 250, height: 250, justifyContent: 'center', alignSelf: 'center', backgroundColor: '#fff', borderRadius: 10, marginTop: 100}}>
-              
-              <Text style = {{fontWeight: '700', textAlign: 'center', margin: 10}}>{this.state.food}</Text>
-              <Text style = {{fontWeight: '500', textAlign: 'center', margin: 10}}>Calories: {this.state.nutrition.calories}</Text>
-              <Text style = {{fontWeight: '500', textAlign: 'center', margin: 10}}>Carbs: {this.state.nutrition.carbs}</Text>
-              <Text style = {{fontWeight: '500', textAlign: 'center', margin: 10}}>Protein: {this.state.nutrition.protein}</Text>
-              <Text style = {{fontWeight: '500', textAlign: 'center', margin: 10}}>Fat: {this.state.nutrition.fat}</Text>
-              <View style = {{flexDirection: 'row', width: 250, height: 40}}>
-                  <TouchableOpacity style = {{width: 125, height: 40, backgroundColor: '#1261a0'}}
-                  onPress = {() => this.setState({viewModal: false})}> 
-                      <Text style = {{fontWeight: '500', textAlign: 'center', margin: 15}}>Cancel</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style = {{width: 125, height: 40, backgroundColor: 'lightgray'}}
-                   onPress = {() => {
-                     this.setState({viewModal: false})
-                     this._storeData({
-                       food: this.state.food,
-                       calories: this.state.nutrition.calories,
-                       carbs: this.state.nutrition.carbs,
-                       protein: this.state.nutrition.protein,
-                       fat: this.state.nutrition.fat,
-                       timestamp: Date.now()
-                     })
-                     }}>
-                     <Text style = {{fontWeight: '500', textAlign: 'center', margin: 10}}>Add</Text>
-                  </TouchableOpacity>
-              </View>
-
-              </View>
+            >
+            <View  style = {{width: 200, height: 200, justifyContent: 'center', alignSelf: 'center', backgroundColor: '#fff', borderRadius: 10, marginTop: 100}}>
+            {this.state.indicating && <ActivityIndicator animating = {this.state.indicating}/>}
+            {!this.state.indicating &&
+            <View>
+            <TouchableOpacity style = {{width: 100}}
+              onPress = {() => this.setState({viewModal: false})}> 
+                  <Icon size = {24} color = {'#143f67'} name="cancel"></Icon>
+            </TouchableOpacity>
+            <Text style = {{fontWeight: '700', textAlign: 'center', margin: 5}}>{(this.state.food).charAt(0).toUpperCase() + (this.state.food).slice(1)}</Text>
+            <Text style = {{fontWeight: '300', textAlign: 'center', margin: 5}}>Calories: {parseInt(this.state.nutrition.calories)}</Text>
+            <Text style = {{fontWeight: '300', textAlign: 'center', margin: 5}}>Carbs: {this.state.nutrition.carbs}g</Text>
+            <Text style = {{fontWeight: '300', textAlign: 'center', margin: 5}}>Protein: {this.state.nutrition.protein}g</Text>
+            <Text style = {{fontWeight: '300', textAlign: 'center', margin: 5}}>Fat: {this.state.nutrition.fat}g</Text>
+            <View style = {{flexDirection: 'row', width: 200, height: 40, justifyContent: 'space-around'}}>
+                <TouchableOpacity style = {{width: 100, height: 40, borderRadius: 10}}
+                 onPress = {async () => {
+                   await this.setState({viewModal: false})
+                   await this._storeData({
+                     food: this.state.food,
+                     calories: this.state.nutrition.calories,
+                     carbs: this.state.nutrition.carbs,
+                     protein: this.state.nutrition.protein,
+                     fat: this.state.nutrition.fat,
+                     timestamp: Date.now()
+                   })
+                   await this.props.navigation.navigate("List")
+                   }}>
+                   <Icon size = {36} color = {'#4794dc'} name="add-circle" style = {{alignSelf: 'center'}}></Icon>
+                </TouchableOpacity>
+                </View>
+                
+            </View>
+                  }
+            </View>
+            
             
             </Modal>
             
             <ImageBackground
              source = {{uri: this.state.uri}}
-             style = {{width: Dimensions.get('window').width, height: Dimensions.get('window').height - 200}}>
+             style = {{width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100}}>
             <SketchCanvas
               ref={ref => {
                 this.pad = ref;
               }}
               eraseComponent={<View style={styles.functionButton}><Text style={{color: 'white'}}>Eraser</Text></View>}
-              style={{position: 'relative', height: Dimensions.get('window').height - 200, opacity: 0.5}}
+              style={{position: 'relative', height: Dimensions.get('window').height - 100, opacity: 0.5}}
               strokeColor={'#1261a0'}
               strokeWidth={4}
               onStrokeEnd = {(path) => {
@@ -240,13 +263,13 @@ class Home extends Component
 
               }}
             />  
-            <View style = {{width: this.state.minX, height: Dimensions.get('window').height - 200, backgroundColor: 'gray', position: 'absolute', opacity: 0.8}}>
+            <View style = {{width: this.state.minX, height: Dimensions.get('window').height - 100, backgroundColor: 'black', position: 'absolute', opacity: 0.9}}>
             </View>
-            <View style = {{width: Dimensions.get('window').width - this.state.maxX, height: Dimensions.get('window').height - 200, backgroundColor: 'gray', position: 'absolute', alignSelf: 'flex-end', opacity: 0.8}}>
+            <View style = {{width: Dimensions.get('window').width - this.state.maxX, height: Dimensions.get('window').height - 100, backgroundColor: 'black', position: 'absolute', alignSelf: 'flex-end', opacity: 0.9}}>
             </View>
-            <View style = {{width: Dimensions.get('window').width, height: this.state.minY, backgroundColor: 'gray', position: 'absolute', opacity: 0.8}}>
+            <View style = {{width: Dimensions.get('window').width, height: this.state.minY, backgroundColor: 'black', position: 'absolute', opacity: 0.9}}>
             </View>
-            <View style = {{width: Dimensions.get('window').width, height: Dimensions.get('window').height - 200 - this.state.maxY, backgroundColor: 'gray', position: 'absolute', marginTop: this.state.maxY, opacity: 0.8}}>
+            <View style = {{width: Dimensions.get('window').width, height: Dimensions.get('window').height - 100 - this.state.maxY, backgroundColor: 'black', position: 'absolute', marginTop: this.state.maxY, opacity: 0.9}}>
             </View>
             <ViewShot ref="viewShot" options={{ format: "jpg", quality: 0.9 }}>
               
@@ -254,20 +277,18 @@ class Home extends Component
 
             </ImageBackground> 
             </ViewShot>
-          }
 
-
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center', flexDirection: 'row' }}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <View style={{flexDirection: 'row', justifyContent: 'center', flexDirection: 'row'}}>
+            <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
             <Icon size = {24} color = {'black'} name="camera-alt"></Icon>
-          </TouchableOpacity>
-          {
-            !this.state.camera &&
+            </TouchableOpacity>
             <TouchableOpacity onPress={this.uploadPicture.bind(this)} style={styles.capture}>
             <Icon size = {24} color = {'black'} name="cloud-upload"></Icon>
             </TouchableOpacity>
+            </View>
+
+            </View>
           }
-        </View>
       </View>
         );
       }
@@ -284,12 +305,14 @@ class Home extends Component
           else
           {
             this.setState({camera: true})
+            this.clear()
           }
         }
 
       uploadPicture = async() => {
+        this.pad.clear()
+        this.setState({viewModal: true})
         await this.captureScreen(this.state.minX, this.state.minY)
-        await this.clear() 
       };
     }
   
@@ -302,7 +325,36 @@ class Home extends Component
         header: null,
       }
     },
-    List
+    List : {
+      screen : List,
+      navigationOptions : {
+        headerStyle: {
+          backgroundColor: '#2168ab',
+          borderBottomColor: '#1e1e1e',
+          borderBottomWidth: 1
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTitle: "Nutrition Log"
+      }
+    },
+    Progress : {
+      screen : Progress,
+      navigationOptions : {
+        headerStyle: {
+          backgroundColor: '#2168ab',
+          borderBottomColor: '#1e1e1e',
+          borderBottomWidth: 0
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerTitle: "Goals"
+      }
+    }
   });
 
   class App extends Component
@@ -331,7 +383,7 @@ const styles = StyleSheet.create({
   preview: {
     justifyContent: 'flex-end',
     alignItems: 'center',
-    height: Dimensions.get('window').height - 200
+    height: Dimensions.get('window').height
   },
   capture: {
     flex: 0,
@@ -341,6 +393,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+
   },
   functionButton: {
     marginHorizontal: 2.5, marginVertical: 8, height: 30, width: 60,
--- List.js
+++ List.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Dimensions, FlatList, StyleSheet, Text, View, Modal, AsyncStorage, Image, Alert, TouchableOpacity, KeyboardAvoidingView,} from 'react-native';
+import {Dimensions, FlatList, StyleSheet, Text, View, Modal, AsyncStorage, Image, TouchableOpacity, KeyboardAvoidingView,} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import { SketchCanvas } from '@terrylinla/react-native-sketch-canvas';
 import ViewShot from 'react-native-view-shot'
@@ -12,10 +12,15 @@ var s3 = new AWS.S3({accessKeyId:'AKIA3BSGO4O2CBIRNSMG', secretAccessKey:'50+4dx
 
 export default class List extends Component
 {
+    static navigationOptions = {
+        title: 'Nutrition List',
+      };
+
     constructor(props) {
         super(props);
         this.state = {
-          data: []
+          data: [], 
+          totalCals: 0
         };
       }
 
@@ -23,7 +28,15 @@ export default class List extends Component
       try {
         const value = await AsyncStorage.getItem('data');
         if (value !== null) {
-          this.setState({data: JSON.parse(value)})
+          await this.setState({data: JSON.parse(value).filter(item => item.food != "french_fries")})
+          const data = this.state.data
+          for (var i = 0; i < data.length; i++)
+          {
+            if (new Date(data[i].timestamp).toDateString() == new Date(Date.now()).toDateString())
+            {
+                this.setState({totalCals: this.state.totalCals + data[i].calories})
+            }
+          }
         }
       } catch (error) {
         // Error saving data
@@ -32,52 +45,47 @@ export default class List extends Component
     
     render() {
         return (
-          <View style = {{flex: 1, backgroundColor: 'black', height: Dimensions.get('window').height}}>
+          <View style = {{flex: 1, backgroundColor: '#fff', height: Dimensions.get('window').height}}>
               <FlatList 
                 value = {this.state}
-                data={this.state.data}
+                data={this.state.data.reverse()}
                 renderItem={({item}) => {
                     return (
-                        <View style = {{backgroundColor: 'gray', marginVertical: 2}}>
-                            <Text style = {{color: '#fff', margin: 5, fontWeight: '700'}}>{item.food}</Text>
-                            <Text style = {{color: '#fff', margin: 5}}>Calories: {item.calories}</Text>
-                            <Text style = {{color: '#fff', margin: 5}}>Carbs: {item.carbs}</Text>
-                            <Text style = {{color: '#fff', margin: 5}}>Protein: {item.protein}</Text>
-                            <Text style = {{color: '#fff', margin: 5}}>Fat: {item.fat}</Text>
-                            <Text style = {{color: '#fff', margin: 5}}>{new Date(item.timestamp).toDateString()}</Text>
+                      <View>
+                        <View style = {{backgroundColor: '#fff'}}>
+                        <View style = {{flexDirection: 'row', justifyContent: 'space-between'}}>
+                          <View>
+                          <Text style = {{color: '#2168ab', margin: 5, fontWeight: '400', fontSize: 24}}>{(item.food).charAt(0).toUpperCase() + (item.food).slice(1)}</Text>
+                          <Text style = {{color: '#2168ab', margin: 5, fontSize: 24, fontWeight: '200'}}>Calories: {parseInt(item.calories)}</Text>
+                          </View>
+                          <View>
+                          <Text style = {{color: '#2168ab', margin: 5, fontWeight: '300'}}>Carbs: {item.carbs}g</Text>
+                          <Text style = {{color: '#2168ab', margin: 5, fontWeight: '300'}}>Protein: {item.protein}g</Text>
+                          <Text style = {{color: '#2168ab', margin: 5, fontWeight: '300'}}>Fat: {item.fat}g</Text>
                         </View>
+                        </View>
+                      </View>
+                      </View>
                     )
                 }}
                 keyExtractor={item => item.timestamp}
                 horizontal = {false}
                 showsVerticalScrollIndicator = {true}
-                ListHeaderComponent = {(
-                    <Text style = {{textAlign: 'center', fontWeight: '700', fontSize: 24, color: '#fff', marginTop: 30}}>Nutrition Log</Text>
-                )}
+                ListHeaderComponent = {
+                  <View>
+                  <View style = {{flexDirection: 'row', justifyContent: 'space-between'}}>
+                     <Text style = {{color: '#2168ab', padding: 5, backgroundColor: '#fff', fontWeight: '300'}}>Today's Intake:</Text>
+                     <Text style = {{color: '#2168ab', padding: 5, backgroundColor: '#fff', fontWeight: '300'}}>{parseInt(this.state.totalCals)} Calories</Text>
+                     
+                 </View>
+                 <View style = {{width: Dimensions.get('window').width, height: .5, backgroundColor: '#2168ab'}}></View>
+                 </View>
+              }
+                ItemSeparatorComponent = {() => {return (<View style = {{width: Dimensions.get('window').width, height: .5, backgroundColor: '#2168ab'}}></View>)}}
                 />
           </View>
         );
       }
-    
-    
-      takePicture = async() => {
-        if (this.state.camera) {
-          const options = { quality: 0.5, base64: true };
-          const data = await this.camera.takePictureAsync(options);
-          this.setState({camera: false, uri: data.uri, base64: data.base64})
-
-          var uploadUri = Platform.OS === 'ios' ? data.uri.replace('file://', '') : uri
-        }
-          else
-          {
-            this.setState({camera: true})
-          }
-        }
-
-      uploadPicture = async() => {
-        await this.captureScreen(this.state.minX, this.state.minY)
-        await this.clear() 
-      };
     }
   
 
