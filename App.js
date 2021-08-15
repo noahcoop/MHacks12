7953a3994dfee2ed409a6bcbd818f375598d4c56
Noah Cooper <44952580+noahcoop@users.noreply.github.com>
2021-08-15 14:16:56 -0400

    Update App.js

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ console.ignoredYellowBox = ['Warning: Each', 'Warning: Failed'];
 
 
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3({accessKeyId:'AKIA3BSGO4O2CBIRNSMG', secretAccessKey:'50+4dxnzS/3NMkhQnaNfAnjAWScdSYlv1qKPMuVS', region:'us-east-1'});
+var s3 = new AWS.S3({accessKeyId:'<YOUR_S3_ACCESS_ID>', secretAccessKey:'<YOUR_S3_SECRET_ACCESS_KEY>', region:'us-east-1'});
 
 class Home extends Component
 {
